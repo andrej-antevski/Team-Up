6c9b2df38d7c8ac75b7637b119465b7b715db8af
Anto <andrej.antevski99@gmail.com>
2020-09-21 18:56:20 +0200

    Add project files.

---
+++ Team-Up/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Team_Up.Startup))]
+namespace Team_Up
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
