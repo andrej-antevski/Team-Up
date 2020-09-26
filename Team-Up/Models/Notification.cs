c936f0dc333f9d6cf23815cf1f82f24ce54f5cd7
Luka <luka_nesevski@hotmail.com>
2020-09-26 14:24:24 +0200

    Add Models

---
+++ Team-Up/Models/Notification.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Team_Up.Models
+{
+    public class Notification
+    {
+ 
+        public Notification ()
+        {
+            this.Recievers = new List<ApplicationUser>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name ="Date Sent")]
+        [DataType(DataType.DateTime)]
+        public DateTime Date { get; set; }
+
+        [Required]
+        public ApplicationUser Sender { get; set; }
+
+        [Required]
+        public virtual ICollection<ApplicationUser> Recievers { get; set; }
+
+        [Required]
+        [Display(Name ="Notification Message")]
+        public string NotificationMessage { get; set; }
+
+        [Required]
+        public bool Seen { get; set; }
+
+    }
+}
